afffae75f6e1498c952d50ddca8dc6bbaff7fd22
Hudson Brendon <contato.hudsonbrendon@gmail.com>
2018-02-21 21:45:47 -0300

    Add args in _get_urls

--- pyudemy/udemy.py
+++ pyudemy/udemy.py
@@ -1,3 +1,4 @@
+import operator
 import requests
 
 from requests.auth import HTTPBasicAuth
@@ -8,20 +9,20 @@ class Udemy(object):
     def __init__(self, client_id, client_secret):
         self.client_id = client_id
         self.client_secret = client_secret
-        self._URL = 'https://www.udemy.com/api-2.0/{}'
+        self._URL = 'https://www.udemy.com/api-2.0/{}/{}'
 
-    def _get_url(self, url):
-        return self._URL.format(url)
+    def _get_url(self, resource, **kwargs):
+        url = self._URL.format(resource, '?')
+        for param, value in sorted(kwargs.items(), key=operator.itemgetter(0)):
+            if value:
+                url += '{}={}&'.format(param, value)
+        return url
 
     @property
     def _authentication(self):
         auth = HTTPBasicAuth(self.client_id, self.client_secret)
         return auth
 
-    def courses(self, page=None, page_size=None, search=None, category=None,
-                subcategory=None, price=None, is_affiliate_agreed=None,
-                is_fixed_priced_deals_agreed=None, is_percentage_deals_agreed=None,
-                language=None, has_closed_caption=None, has_coding_exercises=None,
-                has_simple_quiz=None, instructional_level=None, ordering=None):
-        courses = requests.get(self._get_url('courses/'), auth=self._authentication).json()
+    def courses(self, **kwargs):
+        courses = requests.get(self._get_url('courses', **kwargs), auth=self._authentication).json()
         return courses
