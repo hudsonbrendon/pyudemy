0d09f29182cf1168c1ac86bbf8b01812904c47e1
Hudson Brendon <contato.hudsonbrendon@gmail.com>
2021-09-24 01:57:00 -0300

    Updated package configs

--- setup.py
+++ setup.py
@@ -1,15 +1,26 @@
-from setuptools import setup
+import setuptools
 
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
 
-setup(name='pyudemy',
-      version='0.3',
-      description='Simple integrate of API udemy.com with python',
-      url='https://github.com/hudsonbrendon/pyudemy',
-      author='Hudson Brendon',
-      author_email='contato.hudsonbrendon@gmail.com',
-      license='MIT',
-      packages=['pyudemy'],
-      install_requires=[
-          'requests',
-      ],
-      zip_safe=False)
+setuptools.setup(
+    name="pyudemy",
+    version="1.0.0",
+    author="Hudson Brendon",
+    author_email="contato.hudsonbrendon@gmail.com",
+    description="A small example package",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/hudsonbrendon/pyudemy",
+    project_urls={
+        "Bug Tracker": "https://github.com/hudsonbrendon/pyudemy/issues",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    package_dir={"": "pyudemy"},
+    packages=setuptools.find_packages(where="pyudemy"),
+    python_requires=">=3.8",
+)
