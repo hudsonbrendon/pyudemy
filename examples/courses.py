296ed03ec96803225e4b5ea138d6bbff9c4093ae
Hudson Brendon <contato.hudsonbrendon@gmail.com>
2021-09-24 16:30:36 -0300

    Fix black

--- examples/courses.py
+++ examples/courses.py
@@ -1,9 +1,8 @@
-from pyudemy import Udemy
 from decouple import config
+from pyudemy import Udemy
 
-
-client_id = config('CLIENT_ID')
-client_secret = config('CLIENT_SECRET')
+client_id = config("CLIENT_ID")
+client_secret = config("CLIENT_SECRET")
 
 udemy = Udemy(client_id, client_secret)
 
