a7a425e426bf16bbea60947c85762455cf13b8c2
JamMassey <33434582+JamMassey@users.noreply.github.com>
2022-03-31 03:41:27 +0100

    Showing the bug fix and new functionality

--- examples/courses.py
+++ examples/courses.py
@@ -8,3 +8,13 @@ client_secret = config('CLIENT_SECRET')
 udemy = Udemy(client_id, client_secret)
 
 print(udemy.courses())
+
+
+test_filters = [{ #Can be used to control return data from API
+ "Object": "course",
+ "Setting": "@min",
+ "Additions": ["description"],   
+ "Minus": ["title"]
+ }]
+
+print(subcategory = "Cryptocurrency & Blockchain", udemy.courses(fields = test_filters)) #Bug would previously occur for categories with & in title
